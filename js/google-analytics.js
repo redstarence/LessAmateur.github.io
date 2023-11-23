0b5a8503f2ba52a0cc660f9d0d1dffee03d422d6
redstarence <hongsewuxinglr@gmail.com>
2023-11-24 00:17:35 +0800

    Site updated: 2023-11-24 00:17:35

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,9 +1,13 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-03WZNLJQXX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-03WZNLJQXX');
-</script>
+(function(i, s, o, g, r, a, m) {
+  i[‘GoogleAnalyticsObject’] = r;
+  i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, ‘script’, ‘https://www.google-analytics.com/analytics.js’, ‘ga’);
+ga(‘create’, ‘G-03WZNLJQXX’, ‘auto’);
+ga(‘send’, ‘pageview’);
